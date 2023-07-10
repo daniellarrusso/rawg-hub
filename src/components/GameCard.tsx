26217f3edef793ae497a9c3331f3dfb51d843872
Nathan Adams <npadams21@gmail.com>
2023-07-10 16:32:38 +0100

    clean up game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -5,21 +5,23 @@ import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../utilities/image-url';
 
 interface Props {
-  game: Game;
+    game: Game;
 }
 const GameCard = ({ game }: Props) => {
-  return (
-    <Card>
-      <Image src={getCroppedImageUrl(game.background_image)}></Image>
-      <CardBody>
-        <Heading fontSize={'2xl'}>{game.name}</Heading>
-        <HStack justifyContent={'space-between'}>
-          <PlatformIconList platforms={game.parent_platforms.map((p) => p.platform)} />
-          <CriticScore score={game.metacritic} />
-        </HStack>
-      </CardBody>
-    </Card>
-  );
+    return (
+        <Card>
+            <Image src={getCroppedImageUrl(game.background_image)}></Image>
+            <CardBody>
+                <HStack marginBottom={3} justifyContent={'space-between'}>
+                    <PlatformIconList
+                        platforms={game.parent_platforms.map((p) => p.platform)}
+                    />
+                    <CriticScore score={game.metacritic} />
+                </HStack>
+                <Heading fontSize={'2xl'}>{game.name}</Heading>
+            </CardBody>
+        </Card>
+    );
 };
 
 export default GameCard;
--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -16,7 +16,7 @@ const GameGrid = ({ gameQuery }: Props) => {
         <div>
             {error && <Text>{error}</Text>}
             <ul>
-                <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 5 }} spacing={3}>
+                <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={6}>
                     {isLoading &&
                         skeletons.map((skeleton) => (
                             <GameCardContainer key={skeleton}>
