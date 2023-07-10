40272990c1f07ccaf24357e05e4afbbd4be97f25
Nathan Adams <npadams21@gmail.com>
2023-07-10 15:34:35 +0100

    build search pinput

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,15 +1,17 @@
 import { HStack, Image, Text } from '@chakra-ui/react';
 import logo from '../assets/logo.webp';
 import ColorModeSwitch from './ColorModeSwitch';
+import SearchInput from './SearchInput';
 
 const NavBar = () => {
-  return (
-    <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px"></Image>
-      <Text>NavBar</Text>
-      <ColorModeSwitch />
-    </HStack>
-  );
+    return (
+        <HStack padding="10px">
+            <Image src={logo} boxSize="60px"></Image>
+            <SearchInput />
+            <Text>NavBar</Text>
+            <ColorModeSwitch />
+        </HStack>
+    );
 };
 
 export default NavBar;
